995bfda5988c4fbd8109a0e2585bb10ebfbd5af2
Jeff Eaton <jeff@eaton.fyi>
2023-08-22 13:39:32 -0500

    Cleaning things up, removing the annoying msg

--- app.js
+++ app.js
@@ -6,43 +6,21 @@ For the companion getting started setup guide,
 see: https://slack.dev/bolt-js/tutorial/getting-started 
 */
 
+const GIT_REVISION = process.env.GIT_REV;
+
 // Initializes your app with your bot token and app token
-// Pretty sure we set up the tokens correctly. Fingers crossed.
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
   socketMode: true,
-  appToken: process.env.SLACK_APP_TOKEN
+  appToken: process.env.SLACK_APP_TOKEN,
+  port: process.env.PORT || 3000
 });
 
 // Listens to incoming messages that contain "hello"
-app.message('hello', async ({ message, say }) => {
+app.message('autobot', async ({ message, say }) => {
   // say() sends a message to the channel where the event was triggered
-  await say({
-    blocks: [
-      {
-        "type": "section",
-        "text": {
-          "type": "mrkdwn",
-          "text": `Hey there <@${message.user}>!`
-        },
-        "accessory": {
-          "type": "button",
-          "text": {
-            "type": "plain_text",
-            "text": "Click Me"
-          },
-          "action_id": "button_click"
-        }
-      }
-    ],
-    text: `Hey there <@${message.user}>!`
-  });
-});
-
-app.action('button_click', async ({ body, ack, say }) => {
-  // Acknowledge the action
-  await ack();
-  await say(`<@${body.user.id}> clicked the button`);
+  await say(`What's that, <@${message.user}>?`);
 });
 
 (async () => {
